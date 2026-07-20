5a7c15455aa1b28c31ebd5d90818f03aad8eab10
Himanshu <41346368+Jain2098@users.noreply.github.com>
2026-07-20 10:32:33 -0400

    add db context with a categories table

---
+++ EquipTrack/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EquipTrack.Models;
+
+namespace EquipTrack.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<Asset> Assets { get; set; }
+}
