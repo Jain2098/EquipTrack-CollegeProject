bac7245c8711e193626e09bfb482083ff0931d91
Himanshu <41346368+Jain2098@users.noreply.github.com>
2026-08-10 19:13:12 -0400

    did a refactor to improve the ui across the whole app

---
+++ EquipTrack/Models/AssetStatusExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace EquipTrack.Models;
+
+public static class AssetStatusExtensions
+{
+    public static string ToBadgeClass(this AssetStatus status) => status switch
+    {
+        AssetStatus.Active => "bg-success",
+        AssetStatus.Decommissioned => "bg-secondary",
+        AssetStatus.Recycled => "bg-dark",
+        _ => "bg-light"
+    };
+}
